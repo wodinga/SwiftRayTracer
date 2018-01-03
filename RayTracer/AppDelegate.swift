993d5697c2514fe509a6a6bbc9e6dfad5262bf5d
Rafael Garcia <1337technomage@gmail.com>
2018-01-02 20:49:18 -0500

    Initial Commit

---
+++ RayTracer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RayTracer
+//
+//  Created by David Garcia on 1/2/18.
+//  Copyright © 2018 Ayy Lmao LLC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RayTracer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RayTracer
+//
+//  Created by David Garcia on 1/2/18.
+//  Copyright © 2018 Ayy Lmao LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
