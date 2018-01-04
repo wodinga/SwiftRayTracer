60eaabd34cefe5590f7d2c121c5c8c7debe2e6c9
David Garcia <dgarcia32@gatech.edu>
2018-01-04 18:02:43 -0500

    Updated project to replace vec3 with double3

--- ray.swift
+++ ray.swift
@@ -7,36 +7,46 @@
 //
 
 import Foundation
+import simd
+
+public func dot (_ left: double3, _ right: double3) -> Double {
+    return left.x * right.x + left.y * right.y + left.z * right.z
+}
+
+public func unit_vector (_ v: double3) -> double3 {
+    let length : Double = sqrt(dot(v,v))
+    return double3(x: v.x/length, y:v.y/length, z: v.z/length)
+}
 
 public struct ray {
-    public var origin: vec3
-    public var direction: vec3
-    public func point_at_parameter(t: Double) -> vec3{
+    public var origin: double3
+    public var direction: double3
+    public func point_at_parameter(t: Double) -> double3{
         return origin + t * direction
     }
 
-    public init(origin: vec3, direction: vec3)
+    public init(origin: double3, direction: double3)
     {
         self.origin = origin
         self.direction = direction
     }
 }
 
-public func color(r: ray) -> vec3 {
-    var result = vec3(x: 0,y: 0,z: 0)
-    let minusZ = vec3(x: 0, y:0, z: -1.0)
+public func color(r: ray) -> double3 {
+    var result = double3(x: 0,y: 0,z: 0)
+    let minusZ = double3(x: 0, y:0, z: -1.0)
     var t = hit_sphere(center: minusZ, 0.5, r)
     if t > 0.0 {
         let norm = unit_vector(r.point_at_parameter(t:t) - minusZ)
-        return 0.5 * vec3(x: norm.x + 1.0, y: norm.y + 1.0, z: norm.z + 1.0)
+        return 0.5 * double3(x: norm.x + 1.0, y: norm.y + 1.0, z: norm.z + 1.0)
     }
     let unit_direction = unit_vector(r.direction)
     t = 0.5 * (unit_direction.y + 1.0)
-    result = (1.0 - t) * vec3(x: 1.0, y: 1.0, z:1.0) + t * vec3(x: 0.5, y: 0.7, z:1.0)
+    result = (1.0 - t) * double3(x: 1.0, y: 1.0, z:1.0) + t * double3(x: 0.5, y: 0.7, z:1.0)
     return result
 }
 
-public func hit_sphere(center: vec3, _ radius: Double, _ r: ray) -> Double {
+public func hit_sphere(center: double3, _ radius: Double, _ r: ray) -> Double {
     let oc = r.origin - center
     let a = dot(r.direction, r.direction)
     let b = 2.0 * dot(oc, r.direction)
@@ -48,3 +58,5 @@ public func hit_sphere(center: vec3, _ radius: Double, _ r: ray) -> Double {
         return (-b - sqrt(discriminant)) / (2.0)
     }
 }
+
+
